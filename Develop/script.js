872590b62059e7686d9557861f5844c82e9e3659
Ayden Krohn <krohnayden@gmail.com>
2023-10-04 22:28:52 -0500

    made javascript to generate password

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,65 @@
 // Assignment code here
+var numbers =[0,1,2,3,4,5,6,7,8,9];
+var symbols =["#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","_",];
+var characterCodes = Array.from(Array(26)).map( (_, i)=> i+97);
+var lowercaseLetters = characterCodes.map(code => String.fromCharCode(code));
+var uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase());
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let pass = '';
+  let str = numbers + symbols +lowercaseLetters +uppercaseLetters;
+  for (let i = 1; i <= 8; i++) {
+      let char = Math.floor(Math.random()
+          * str.length + 1);
 
-  passwordText.value = password;
+      pass += str.charAt(char)
+  }
 
+  return pass;
 }
 
+function passwordOptions() {
+  if (numbers && symbols && lowercaseLetters && uppercaseLetters) {
+    console.log(pass)
+  } if (numbers && symbols && lowercaseLetters) {
+
+  }
+  }
+console.log(writePassword());
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write password to the #password input
+// function writePassword() {
+//   let pass = '';
+//   let str = options;
+
+//   for (let i =1; i <= 8; i++) {
+//     let char = Math.floor(Math.random() * str.length +1);
+//   }
+
+//   return pass;
+//   // var password = generatePassword();
+//   // var passwordText = document.querySelector("#password");
+//   // var index = Math.floor(Math.random() * options.length);
+//   // options.value = password;
+// }
+
+// console.log(writePassword());
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword());
